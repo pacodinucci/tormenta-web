d15faf51a15700aa7dd2a09acf3b477085b63827
pacodinucci <franciscoldinucci@gmail.com>
2025-05-29 17:32:31 -0300

    props fail

--- app/admin/products/[productId]/page.tsx
+++ app/admin/products/[productId]/page.tsx
@@ -3,8 +3,7 @@ import AddProductForm from "@/components/AddProductForm";
 import React from "react";
 
 const ProductIdPage = async ({ params }: { params: { productId: string } }) => {
-  const productId = params?.productId;
-  const product = await getProductById(productId);
+  const product = await getProductById(params.productId);
 
   return (
     <div>
