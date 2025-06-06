d38b402a610743904497ec9fd18170325c7695bb
pacodinucci <franciscoldinucci@gmail.com>
2025-06-06 14:04:13 -0300

    basket on succes page test

--- components/SuccessComponent.tsx
+++ components/SuccessComponent.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import OutlineShadowButton from "./OutlineShadowButton";
+import { ShoppingBasket } from "lucide-react";
+import { useCart } from "@/store/cart";
 
 const SuccessComponent = () => {
+  const items = useCart((state) => state.items);
+  const totalQuantity = items.reduce((sum, item) => sum + item.quantity, 0);
   return (
     <div className="flex flex-col gap-4">
       <h1 style={{ fontFamily: "var(--font-impact)" }} className="text-5xl">
@@ -12,6 +17,15 @@ const SuccessComponent = () => {
       <h3 style={{ fontFamily: "var(--font-franklin)" }} className="text-3xl">
         Ante cualquier duda o consulta, escribe a hola@tormenta.mx
       </h3>
+      <OutlineShadowButton
+        // onClick={() => router.push("/cart")}
+        className="bg-emerald-700"
+      >
+        <div className="flex items-center gap-2">
+          <ShoppingBasket />
+          <span>{totalQuantity}</span>
+        </div>
+      </OutlineShadowButton>
     </div>
   );
 };
