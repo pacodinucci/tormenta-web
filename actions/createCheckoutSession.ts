2c0886ac45efc67a69f514b78117c0f069bf3983
pacodinucci <franciscoldinucci@gmail.com>
2025-06-16 18:37:45 -0300

    stripe redirect fix

--- actions/createCheckoutSession.ts
+++ actions/createCheckoutSession.ts
@@ -39,12 +39,11 @@ export async function createCheckoutSession(
       metadata,
       mode: "payment",
       allow_promotion_codes: true,
-      success_url: `https://www.tormenta.mx/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`,
-      // success_url: `${
-      //   `https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL
-      // }/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${
-      //   metadata.orderNumber
-      // }`,
+      success_url: `${
+        `https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL
+      }/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${
+        metadata.orderNumber
+      }`,
       cancel_url: `${
         `https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL
       }/cart`,
